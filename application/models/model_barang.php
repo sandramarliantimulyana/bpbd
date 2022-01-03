e7f5a478f68200426dd68e6ee60ff225f135ad3a
sandramarliantimulyana <my>
2022-01-03 14:51:41 +0700

    join barang masuk dan keluar

--- application/models/model_barang.php
+++ application/models/model_barang.php
@@ -16,4 +16,13 @@ class Model_barang extends CI_Model
         $this->db->join('sumber', 'sumber.id_sumber=barang_masuk.id_sumber');
         return $this->db->get()->result_array();
     }
+
+    //join table barang_masuk with barang_keluar
+    public function join_barangmasuk_barangkeluar()
+    {
+        $this->db->select('*');
+        $this->db->from('barang_masuk');
+        $this->db->join('barang_keluar', 'barang_masuk.id_barang_masuk=barang_keluar.id_barang_masuk');
+        return $this->db->get()->result_array();
+    }
 }
