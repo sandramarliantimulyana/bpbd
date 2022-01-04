254af833139b67e07010068a282049c9f287bef9
sandramarliantimulyana <my>
2022-01-04 16:14:10 +0700

    update tapi can bisa nampil

--- application/views/dashboard/i_keluar.php
+++ application/views/dashboard/i_keluar.php
@@ -6,21 +6,9 @@
                 <h2>
                     Barang Keluar
                 </h2>
-                <ul class="header-dropdown m-r--5">
-                    <li class="dropdown">
-                        <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                            <i class="material-icons">more_vert</i>
-                        </a>
-                        <ul class="dropdown-menu pull-right">
-                            <li><a href="javascript:void(0);">Action</a></li>
-                            <li><a href="javascript:void(0);">Another action</a></li>
-                            <li><a href="javascript:void(0);">Something else here</a></li>
-                        </ul>
-                    </li>
-                </ul>
             </div>
             <div class="body">
-                <form>
+                <form method="post" action="<?= base_url('dashboard/i_masuk') ?>">
                     <label for="tgl_keluar">Tanggal Keluar</label>
                     <div class="form-group">
                         <div class="form-line">
