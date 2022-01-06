879c9410e8664052efe3f750a7d8721591865847
sandramarliantimulyana <my>
2022-01-06 15:28:28 +0700

    gambar teu nampil

--- application/views/template/index.php
+++ application/views/template/index.php
@@ -73,7 +73,7 @@
             <!-- User Info -->
             <div class="user-info">
                 <div class="image">
-                    <img src="<?= base_url('template/') ?>images/user.png" width="48" height="48" alt="User" />
+                    <img src="<?= base_url('views/gambar') ?>images/bpbd.png" width="48" height="48" alt="User" />
                 </div>
                 <div class="info-container">
                     <div class="name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">John Doe</div>
