7bd130a5b53048a9df4ee9e6c5f68e79d937a8d4
sandramarliantimulyana <my>
2022-01-16 00:20:06 +0700

    update tabel

--- application/views/dashboard/stok.php
+++ application/views/dashboard/stok.php
@@ -1,22 +1,30 @@
-<div class="body">
-    <div class="table-responsive">
-        <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
-            <thead>
-                <tr>
-                    <th>Nama Barang</th>
-                    <th>Satuan</th>
-                    <th>Jumlah</th>
-                </tr>
-            </thead>
+<div class="row clearfix">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="card">
+            <div class="header">
+                <!-- <a href="<?= base_url('dashboard/i_keluar') ?>" class="btn btn-primary"> Tambah Barang Keluar </a> -->
+            </div>
+            <div class="body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
+                        <thead>
+                            <tr>
+                                <th>Nama Barang</th>
+                                <th>Satuan</th>
+                                <th>Jumlah</th>
+                            </tr>
+                        </thead>
 
-            <tbody>
-                <?php
-                foreach ($tampil as $t) : ?>
-                    <tr>
-                        <td><?= $t['nama_barang']; ?></td>
-                        <td><?= $t['satuan']; ?></td>
-                        <td><?= $t['jml']; ?></td>
-                        <!-- <td>
+                        <tbody>
+                            <?php
+                            foreach ($tampil as $t) : ?>
+                                <tr>
+
+                                    <td><?= $t['nama_barang']; ?></td>
+                                    <td><?= $t['satuan']; ?></td>
+                                    <td><?= $t['jml']; ?></td>
+
+                                    <!-- <td>
                                         <a href="<?= base_url('dashboard/hapus_keluar/' . $t['id_keluar']) ?>" class="btn btn-danger btn-sm" onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')">
                                             Hapus
                                         </a>
@@ -24,20 +32,20 @@
                                             Edit
                                         </a>
                                     </td> -->
-                    </tr>
-                <?php
-                endforeach; ?>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td colspan="5">
-                        <ul class="pagination float-right"></ul>
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
+                                </tr>
+                            <?php
+                            endforeach; ?>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="5">
+                                    <ul class="pagination float-right"></ul>
+                                </td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-</div>
-</div>
 </div>
