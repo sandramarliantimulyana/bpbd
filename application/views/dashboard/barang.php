55ad2d6e85a0a4334f85d079a8f27c48b5a5e1be
sandramarliantimulyana <my>
2022-01-17 21:42:57 +0700

    barang oke sudah tampil

--- application/views/dashboard/barang.php
+++ application/views/dashboard/barang.php
@@ -2,40 +2,36 @@
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <div class="card">
             <div class="header">
-                <!-- <a href="" class="btn btn-primary"> Tambah Barang </a> -->
+                <!-- <a href="<?= base_url('dashboard/i_barang') ?>" class="btn btn-primary"> Tambah Barang </a> -->
             </div>
             <div class="body">
                 <div class="table-responsive">
                     <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
                         <thead>
                             <tr>
-
+                                <th>No</th>
                                 <th>Nama Barang</th>
                                 <th>Satuan</th>
-                                <!-- <th>Kategori</th> -->
-                                <!-- <th>Standar Minimal</th> -->
                             </tr>
                         </thead>
 
                         <tbody>
-                            <?php
+                            <?php $no = 1;
                             foreach ($tampil as $t) : ?>
                                 <tr>
-
+                                    <td><?= $no; ?></td>
                                     <td><?= $t['nama_barang']; ?></td>
                                     <td><?= $t['satuan']; ?></td>
-                                    <!-- <td><?= $t['nama_kategori']; ?></td> -->
-                                    <!-- <td><?= $t['tujuan']; ?></td> -->
                                     <!-- <td>
-                                        <a href="<?= base_url('dashboard/hapus_keluar/' . $t['id_keluar']) ?>" class="btn btn-danger btn-sm" onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')">
+                                        <a href="<?= base_url('dashboard/hapus_barang/' . $t['id_masuk']) ?>" class="btn btn-danger btn-sm" onclick="return confirm('Apakah anda yakin ingin menghapus data ini?')">
                                             Hapus
                                         </a>
-                                        <a href="<?= base_url('dashboard/edit_keluar/' . $t['id_keluar']) ?>" class="btn btn-success btn-sm">
+                                        <a href="<?= base_url('dashboard/edit_barang/' . $t['id_masuk']) ?>" class="btn btn-success btn-sm">
                                             Edit
                                         </a>
                                     </td> -->
                                 </tr>
-                            <?php
+                            <?php $no++;
                             endforeach; ?>
                         </tbody>
                         <tfoot>
